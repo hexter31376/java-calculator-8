c77ad367fc0104aaba345d68deee3be7aee1a39f
teamplay <67694293+hexter31376@users.noreply.github.com>
2025-10-20 03:31:21 +0900

    feat(Application.java): 어플리케이션 클래스 재구성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,18 +1,10 @@
 package calculator;
 
-import calculator.config.ApplicationConfig;
-import calculator.exception.GlobalExceptionHandler;
-import calculator.presentation.CalculatorController;
+import calculator.config.AppConfig;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        // JVM 레벨 전역 예외 핸들러 실행
-        Thread.setDefaultUncaughtExceptionHandler(new GlobalExceptionHandler());
-
-        ApplicationConfig applicationConfig = new ApplicationConfig();
-        CalculatorController calculatorController = applicationConfig.calculatorController();
-
-        // TODO: 메인 로직 스타터 호출
+        AppConfig appConfig = new AppConfig();
+        appConfig.run();
     }
 }
