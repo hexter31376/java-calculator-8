88e4132ea580dd6fff797ca9335f1d5b2607da62
teamplay <67694293+hexter31376@users.noreply.github.com>
2025-10-20 16:46:39 +0900

    refactor(AppConfig.java): ParserService 이름을 StringParser로 변경

--- src/main/java/calculator/config/AppConfig.java
+++ src/main/java/calculator/config/AppConfig.java
@@ -1,15 +1,15 @@
 package calculator.config;
 
 import calculator.application.CalculatorService;
-import calculator.domain.parsor.ParserService;
+import calculator.domain.parsor.StringParser;
 import calculator.presentation.CalculatorController;
 import calculator.view.InputView;
 import calculator.view.OutputView;
 
 public class AppConfig {
     // service
-    private final ParserService parserService =  new ParserService();
-    private final CalculatorService calculatorService = new CalculatorService(parserService);
+    private final StringParser stringParser =  new StringParser();
+    private final CalculatorService calculatorService = new CalculatorService(stringParser);
 
     // view
     private final InputView inputView = new InputView();
