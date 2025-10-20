9414f135d4205d44adbfeb51d71e65260c9c267b
teamplay <67694293+hexter31376@users.noreply.github.com>
2025-10-20 14:02:44 +0900

    refactor(AppConfig.java): 철자 교정

--- src/main/java/calculator/config/AppConfig.java
+++ src/main/java/calculator/config/AppConfig.java
@@ -1,15 +1,15 @@
 package calculator.config;
 
 import calculator.application.CalculatorService;
-import calculator.domain.parsor.ParsorService;
+import calculator.domain.parsor.ParserService;
 import calculator.presentation.CalculatorController;
 import calculator.view.InputView;
 import calculator.view.OutputView;
 
 public class AppConfig {
     // service
-    private final ParsorService parsorService =  new ParsorService();
-    private final CalculatorService calculatorService = new CalculatorService(parsorService);
+    private final ParserService parserService =  new ParserService();
+    private final CalculatorService calculatorService = new CalculatorService(parserService);
 
     // view
     private final InputView inputView = new InputView();
