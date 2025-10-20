7a790a26a976a9760fbe4e99746b5ec70c680ad8
teamplay <67694293+hexter31376@users.noreply.github.com>
2025-10-20 20:33:31 +0900

    refactor(StringParser.java): 순수 계산부 삽입

--- src/main/java/calculator/domain/parsor/StringParser.java
+++ src/main/java/calculator/domain/parsor/StringParser.java
@@ -10,11 +10,15 @@ public class StringParser {
 
     private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)" + Pattern.quote("\\n" ) + (".*"));
 
+
     public DelimiterFilteredResult parse(String rawText) {
         if (rawText.startsWith("//")) {
             String customDelimiter = getCustomDelimiter(rawText);
             String regex = Delimiters.of(customDelimiter).buildRegex();
-            return new DelimiterFilteredResult(regex, rawText);
+            // 순수 연산부 얻기
+            String delimiterNumberText = getDelimiterNumberText(rawText);
+
+            return new DelimiterFilteredResult(regex, delimiterNumberText);
         } else {
             String defalutRegex = Delimiters.setDefault().buildRegex();
             return new DelimiterFilteredResult(defalutRegex, rawText);
@@ -27,5 +31,13 @@ public class StringParser {
             throw new IllegalArgumentException("구분자 형식이 잘못되었습니다.");
         }
         return matcher.group(1);
+    }
+
+    private String getDelimiterNumberText (String rawText) {
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(rawText);
+        if (!matcher.find()) {
+            throw new IllegalArgumentException("구분자 형식이 잘못되었습니다.");
+        }
+        return matcher.group(2);
     }
 }
